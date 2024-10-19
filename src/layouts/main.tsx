ee333e948c3670559798177f4907b3a395700387
Mohammad karbalaiemehdi <mohammadkarbalaiemehdi@outlook.com>
2024-10-19 13:29:09 +0330

    fix-ui-better

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -1,24 +1,18 @@
 import { Outlet } from "react-router-dom";
 import { NavBar } from "../components/navbar";
 import Footer from "../components/footer";
-import { AiOutlineArrowRight } from "react-icons/ai";
 
 export const MainLayout: React.FC = () => {
   return (
     <>
-      <div className="w-full flex flex-col bg-[#e9e8e8] mx-auto max-w-[1920px] h-[100vh]">
-          <div className="w-full flex  overflow-hidden">
-            <div className="overflow-visible max-w-[290px] w-full">
+      <div className="w-full flex flex-col bg-[#e9e8e8] mx-auto max-w-[1440px] h-[100vh]"> 
               <NavBar />
-            </div>
-            <div className="overflow-auto max-w-[1900px] w-full ">
-            <button className="text-3xl absolute mt-2 left-[1860px] hover:text-sky-400"><AiOutlineArrowRight /></button>
+              <div className="max-w-[1440px] w-full flex flex-col mx-auto">
               <Outlet />
+              </div>
+            <div className="w-[1440px] flex flex-col justify-end items-end bg-gray-900">
+                <Footer/>
             </div>
-          </div>
-        <div className="w-[1900px] flex flex-col justify-end items-end bg-gray-900">
-            <Footer/>
-        </div>
       </div>
       
 
